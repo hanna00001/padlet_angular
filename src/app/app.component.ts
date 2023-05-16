7f952d61ad6a00ed0b236aaa5b3cdbcfbdf1d2e9
Hanna <s2010456010@fhooe.at>
2023-05-16 17:54:59 +0200

    16.05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Padlet} from "./shared/padlet";
 
 @Component({
   selector: 'bs-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
+  listOn = true;
+  detailsOn = false;
+
+  padlet : Padlet |undefined;
+
+  showList() {
+    this.listOn = true;
+    this.detailsOn = false;
+  }
+  showDetails(padlet: Padlet) {
+    this.padlet = padlet;
+    this.listOn = false;
+    this.detailsOn = true;
+  }
+
   title = 'padlet23';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PadletListComponent } from './padlet-list/padlet-list.component';
+import { PadletListItemComponent } from './padlet-list-item/padlet-list-item.component';
+import { PadletDetailsComponent } from './padlet-details/padlet-details.component';
+import { EntrieItemComponent } from './entrie-item/entrie-item.component';
+import {PadletService} from "./shared/padlet.service";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadletListComponent,
+    PadletListItemComponent,
+    PadletDetailsComponent,
+    EntrieItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
-  providers: [],
+  providers: [PadletService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
